79ca92076861a26d66f8089be72f1fbe9e66eba8
alinec-santos <alinesantoscss17@gmail.com>
2026-05-03 13:48:53 -0300

    chore: configurar servidor express com rota de health

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,16 @@
 // Configuraçoes do express
+
+import express from "express"
+import cors from "cors"
+
+const app = express() //cria o servidor 
+
+app.use(cors()) // permite que o front acesse o back
+app.use(express.json()) //permite receber json no body das requisiçoes 
+
+// rota de teste
+app.get("/health", (req, res) => {
+  return res.status(200).json({ message: "API funcionando" })
+})
+
+export default app
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,9 @@
 // codigo que sobe o servidor
+
+import app from "./app"
+
+const PORT = 3000
+
+app.listen(PORT, () => { //sobe o servidor
+  console.log(` Server running on port ${PORT}`)
+})
